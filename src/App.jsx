53e5f7fbfe1c5fe81ff22da6af8aaa9cbfccb1ec
TRISHAN00 <trishansaha43@gmail.com>
2025-03-03 23:07:17 +0600

    add to cart and print the add to cart in the modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import Sidebar from "./components/Sidebar";
+import { MovieContext } from "./context";
 
 function App() {
+  const [cartData, setCartData] = useState([]);
+
   return (
     <>
-      {/* Header */}
-      <Header />
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        {/* Header */}
+        <Header />
 
-      <main>
-        <div class="container m-auto grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          {/* Sidebar */}
-          <Sidebar />
-          {/* Movie List */}
-          <MovieList />
-        </div>
-      </main>
+        <main>
+          <div class="container m-auto grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            {/* Sidebar */}
+            <Sidebar />
+            {/* Movie List */}
+            <MovieList />
+          </div>
+        </main>
 
-      {/* Footer */}
-      <Footer />
+        {/* Footer */}
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
